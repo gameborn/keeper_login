bf9cf09e3c3f477659b74210135bb3d2a3202bdf
MR.GAMEBORN <gameborn.owner@gmail.com>
2020-10-29 15:15:26 -0400

    Update KeeperApplication.kt

--- app/src/main/java/com/gameborn/keeperlogin/KeeperApplication.kt
+++ app/src/main/java/com/gameborn/keeperlogin/KeeperApplication.kt
@@ -6,7 +6,7 @@ import dagger.hilt.android.HiltAndroidApp
 /**
  * KeeperApplication - required class to initialize Hilt
  * provides options to handle some global dependecies in Application
- * Modules which want to inject data here must me annotated with InstallIn(ApplicationComponent::class)
+ * Modules which want to inject data here must be annotated with InstallIn(ApplicationComponent::class)
  */
 
 @HiltAndroidApp
